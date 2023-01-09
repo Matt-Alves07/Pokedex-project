ef274bccafc56f49134cc00d1ba8c12c27785942
Matheus Alves <mathalves07.winstore@outlook.com>
2023-01-09 18:10:37 -0300

    Included the function to list all the first generation pokémons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     PagesModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+  public getAllPokemons: any;
 
+  constructor(private pokeApiService: PokeapiService) { }
+
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.getAllPokemons = res.results;
+      }
+    );
+  }
 }
