6fb4b47efd396eadefefc4924651f4e4a1a8a95f
Matheus Alves <mathalves07.winstore@outlook.com>
2023-01-09 19:02:12 -0300

    Moved the search component, and made it filter the pokémons of the list

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -8,14 +8,24 @@ import { PokeapiService } from 'src/app/services/pokeapi.service';
 })
 export class PokeListComponent implements OnInit {
   public getAllPokemons: any;
+  private setAllPokemons: any;
 
   constructor(private pokeApiService: PokeapiService) { }
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
       res => {
-        this.getAllPokemons = res.results;
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
       }
     );
+  }
+
+  public search(value: string) {
+    const filter = this.setAllPokemons.filter( ( res: any ) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
   }
 }
