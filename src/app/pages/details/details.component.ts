3559ff056578d1c6cdcc1a2b7411958b11e89fc2
Matheus Alves <mathalves07.winstore@outlook.com>
2023-01-09 20:35:18 -0300

    Finished the pokémon details page

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+
+  constructor(
+    private activatedRoute: ActivatedRoute, private pokeApiService: PokeapiService
+  ) { }
+
+  ngOnInit(): void {
+    this.getPokemon;
+  }
+
+  get getPokemon() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin(pokemon, name).subscribe(
+      res => {
+        this.pokemon = res;
+      }
+    )
+  }
 }
